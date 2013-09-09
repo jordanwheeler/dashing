e0e6a631ac41f0d1fe2c0dffb500b29f2d45dd16
Jordan Wheeler <jordan.wheeler@jadedpixel.com>
2013-09-09 15:58:38 -0400

    use firebase

--- lib/dashing.rb
+++ lib/dashing.rb
@@ -6,7 +6,9 @@ require 'coffee-script'
 require 'sass'
 require 'json'
 require 'yaml'
+require 'firebase'
 
+Firebase.base_uri = 'https://shopify-statly.firebaseio.com/'
 SCHEDULER = Rufus::Scheduler.start_new
 
 set :root, Dir.pwd
@@ -125,6 +127,7 @@ def send_event(id, body, target=nil)
   body[:id] = id
   body[:updatedAt] ||= Time.now.to_i
   event = format_event(body.to_json, target)
+  Firebase.set("#{id}", "#{body.to_json}")
   Sinatra::Application.settings.history[id] = event unless target == 'dashboards'
   Sinatra::Application.settings.connections.each { |out| out << event }
 end
